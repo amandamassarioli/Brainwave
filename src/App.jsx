3cdd6d452822ec82dc7ea78def251f15d68a51bd
AmandaMassarioli <amandamassarioli3@gmail.com>
2025-01-17 03:37:38 -0300

    first changes about the visual.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
-function App() {
-  
+import ButtonGradient from './assets/svg/ButtonGradient'
 
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+const App = () => {
+  return (
+  <>
+  <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+     <ButtonGradient />
+  </>
+  )
 }
 
 export default App

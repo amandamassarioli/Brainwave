b7c0f3f97c75bfec9bbdcfa7e207ffeca8fb7c9a
AmandaMassarioli <amandamassarioli3@gmail.com>
2025-01-17 13:30:51 -0300

    first button and some new tabs not configured.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
-import ButtonGradient from './assets/svg/ButtonGradient'
+import React from 'react';
+import Header from './components/Header';
+import ButtonGradient from './assets/svg/ButtonGradient';
+import Button from "./components/Button";
 
 const App = () => {
   return (
-  <>
-  <h1 className="text-3xl font-bold underline">Hello world!</h1>;
-     <ButtonGradient />
-  </>
-  )
+    <>
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+        <Header />
+      </div>
+      <Button className="mt-10" href="#login">
+        Something
+      </Button>
+      <ButtonGradient />
+    </>
+  );
 }
 
-export default App
+export default App; 
